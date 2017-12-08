1b2f6bec398d37053de480b85eb13fa223de0190
jayrexacilo <acilojayrex@gmail.com>
2017-12-08 13:49:08 +0800

    Boilerplate with SASS Preprocessor setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # geckos-28
+
+## Quick Overview
+
+```sh
+git clone https://github.com/chingu-voyage3/geckos-28.git
+
+cd geckos-28
+
+npm install
+
+npm start
+```
--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles/App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <h1>Hello World!</h1>
       </div>
     );
   }
