2d0949c409e38345af662291ce79e3682dd6111c
jayrexacilo <acilojayrex@gmail.com>
2017-12-16 14:39:43 +0800

    Fullpage background and button that dynamically change background image

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
+
+import { getBackgroundImage } from "../Components/Background";
 import "../styles/App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      bgStyle: {
+        backgroundImage: getBackgroundImage(),
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        minHeight: "100vh"
+      }
+    };
+
+    this.changeBackground = this.changeBackground.bind(this);
+  }
+
+  changeBackground() {
+    this.setState({
+      bgStyle: {
+        ...this.state.bgStyle,
+        backgroundImage: getBackgroundImage()
+      }
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
+      <div className="App" style={this.state.bgStyle}>
+        <button className="changeBackground" onClick={this.changeBackground}>
+          <i className="fa fa-refresh" aria-hidden="true" />
+        </button>
       </div>
     );
   }
