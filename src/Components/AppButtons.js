948daccd96c4828e56f922c484af73f4e042f91f
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-01-24 19:56:34 +0600

    Changed from name to id on li items

--- src/Components/AppButtons.js
+++ src/Components/AppButtons.js
@@ -6,7 +6,7 @@ function AppButtons(props) {
         <div className="appButtons">
             <ul>
                 {props.listOfApps.map(item => 
-                    <li name={item.app} onClick={props.handleClick}>
+                    <li id={item.app} onClick={props.handleClick(item)}>
                         <img src={icon} alt="Click to Open/Close app"/>
                         <p>{item.title}</p>
                     </li>
