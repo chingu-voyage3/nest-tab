70e3232557dc5715d2d35b3bfaa6ad1ad1d6663d
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-01-24 14:33:24 +0600

    Fixed bug of 12th hour showing 0

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,6 +1,6 @@
 export function formatTime() {
     const currentDate = new Date();
-    const currentHour = currentDate.getHours() % 12 === "0" ? "12" : currentDate.getHours() % 12;
+    const currentHour = currentDate.getHours() % 12 === 0 ? "12" : currentDate.getHours() % 12;
     const currentMinute = 
         currentDate.getMinutes() < 10 ? "0" + currentDate.getMinutes() : currentDate.getMinutes();
     const ampm = currentDate.getHours() >= 12 ? "PM" : "AM";
