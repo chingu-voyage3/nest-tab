e9d83ffe394c1b0212c9706a29d52c22852f4822
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-01-27 19:57:25 +0600

    included weather info in day dayInfo component

--- src/Components/DayInfo.js
+++ src/Components/DayInfo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { currentWeekday } from "../utils/currentWeekday";
 
-function DayInfo() {
+function DayInfo(props) {
     const day = currentWeekday(new Date());
-
+    const weatherInfo = props.weather ? " It's "+props.weather.currently.summary+" outside with a temprature of " +props.weather.currently.temperature+"°C" : "";
     return(
-        <p>This is an amazing <strong>{day}</strong>! What will you accomplish today? :)</p>
+        <p className="daySummary">This is an amazing <strong>{day}</strong>!
+        {weatherInfo}<br/>
+        What will you accomplish today? :)</p>
     );
 }
 
