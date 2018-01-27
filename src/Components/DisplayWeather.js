bee840a552c8b1518b0910ad8e1029e1e74f260a
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-01-27 19:57:43 +0600

    removed un-needed component

--- src/Components/DisplayWeather.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function DisplayWeather(props) {
-    return (
-        props.weather ? <p>It's {props.weather.currently.summary} outside with a temprature of {props.weather.currently.temperature}°C</p> : ""
-    );
-}
-
-export default DisplayWeather;
