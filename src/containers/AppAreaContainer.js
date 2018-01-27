361b200d91aa6f64417764e0082002a66517639c
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-01-28 01:01:45 +0600

    imported SetUsername component

--- src/containers/AppAreaContainer.js
+++ src/containers/AppAreaContainer.js
@@ -7,6 +7,7 @@ import Shelf from "../Components/Shelf";
 import AppButtons from "../Components/AppButtons";
 import AppViewer from "./AppViewer";
 import DayInfoContainer from "./DayInfoContainer";
+import SetUserName from "../Components/SetUserName";
 import "../styles/AppArea.css";
 
 
@@ -39,24 +40,42 @@ class AppAreaContainer extends Component {
 
         this.state = {
             appInView: false,
-            currentApp: ""
+            currentApp: "",
+            name: "",
+            isNameSaved: false
         }
 
         this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleClick = param => event => {
-
-        
-
         this.setState({
             appInView: this.state.currentApp === param.component ? !this.state.appInView : true,
             currentApp: param.component
         })
     }
 
+    handleChange(event) {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        localStorage.setItem('name', JSON.stringify(this.state.name));
+        this.setState({
+            isNameSaved: true
+        })
+    }
+
     render() {
-        return(
+        if (localStorage.getItem('name') == null) {
+            return <SetUserName change={this.handleChange} submit={this.handleSubmit} name={this.state.name}/>
+        }
+        else return(
             <div className="appArea">
                 <DayInfoContainer showHide={this.state.appInView ? "dayInfo poof" : "dayInfo"} />
                 <AppViewer appComponent={this.state.currentApp} shouldView={this.state.appInView} dayInfo={<DayInfoContainer />} />
