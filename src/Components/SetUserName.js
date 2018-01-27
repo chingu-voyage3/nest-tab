a84ebc0dc14e1b61b49f8fbff5e0a0defefcd3d9
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-01-28 01:01:01 +0600

    component to add users name

---
+++ src/Components/SetUserName.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../styles/SetUserName.css";
+
+function SetUserName(props) {
+    return(
+        <div className="askName">
+            <h1>Hi there! What should I call you? :)</h1>
+            <form onSubmit={props.submit}>
+                <input type="text" value={props.name} onChange={props.change} />
+                <button type="submit"><i class="material-icons">arrow_forward</i></button>
+            </form>
+        </div>
+    )
+}
+
+export default SetUserName;
