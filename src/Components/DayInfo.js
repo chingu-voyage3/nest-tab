b6f3108f0ada78f70f8a76363f08ef8f521f8775
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-01-28 19:25:23 +0600

    Added time based dynamic greetings

--- src/Components/DayInfo.js
+++ src/Components/DayInfo.js
@@ -4,9 +4,29 @@ import { currentWeekday } from "../utils/currentWeekday";
 function DayInfo(props) {
     const day = currentWeekday(new Date());
     const weatherInfo = props.weather ? " It's "+props.weather.currently.summary+" outside with a temprature of " +props.weather.currently.temperature.toFixed(1)+"°C" : "";
+    const timeOfDay = new Date().getHours();
+    let greeting = "Hi";
+    switch(true) {
+        case (timeOfDay >= 0 && timeOfDay < 12 ):
+            greeting = "Good Morning";
+            break;
+        case (timeOfDay >= 12 && timeOfDay < 16):
+            greeting = "Good Noon";
+            break;
+        case (timeOfDay >= 16 && timeOfDay < 18):
+            greeting = "Good Afternoon"
+            break;
+        case (timeOfDay >= 18 && timeOfDay < 25):
+            greeting = "Good Evening"
+            break;
+        default:
+            greeting = "Hi";
+            break;
+    }
+
     return(
         <div className="daySummary">
-            <h1>Hi, {props.name}!</h1>
+            <h1>{greeting}, {props.name}!</h1>
             <p>This is an amazing <strong>{day}</strong>!
             {weatherInfo}<br/>
             What will you accomplish today? :)</p>
