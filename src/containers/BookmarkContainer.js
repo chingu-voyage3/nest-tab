5daa2d373bb9b787de216c433a4f669381922db9
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-02-07 22:20:08 +0600

    written method to sort folders up top in bookmark list

---
+++ src/containers/BookmarkContainer.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import BookmarkList from '../Components/BookmarkList'; 
+import "../styles/Bookmark.css";
+// import {bookmarkLists} from "./bookObj";
+import { Scrollbars } from 'react-custom-scrollbars';
+// const bookmarkLists = [];
+
+class BookmarkContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarkFetched: false,
+      bookmarks: null
+    }
+
+    this.fetchBookmark = this.fetchBookmark.bind(this);
+  }
+
+  fetchBookmark() {
+  /*eslint-disable no-undef*/
+    chrome.bookmarks.getTree(result => {
+      this.setState({
+        bookmarkFetched: true,
+        bookmarks: result
+      }, () => this.foldersToTop())
+    });
+  }
+  /*eslint-enable no-undef*/
+
+  foldersToTop() {
+    const elements = document.querySelectorAll("div ul li");
+    elements.forEach(item => {
+      item.parentElement.insertBefore(item, null);
+    });
+  }
+
+  componentWillMount() {
+    this.fetchBookmark();
+  }
+
+  folderHandler(event) {
+    event.target.classList.toggle("closed");
+  }
+
+  render() {
+    return(
+      <div>
+        <Scrollbars style={{width: 450, height: 420}}>
+          <BookmarkList isFetched={this.state.bookmarkFetched} bookmarks={this.state.bookmarks} folderHandler={this.folderHandler}/>
+          {/* <BookmarkList folderHandler={this.folderHandler}/> */}
+        </Scrollbars>
+      </div>
+    )
+  }
+}
+
+export default BookmarkContainer;
