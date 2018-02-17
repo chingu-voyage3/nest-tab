49aa5d505678f7ab23f34a6174e58bb7cd55271a
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-02-17 17:24:30 +0600

    extracted container component

--- src/Components/To-Do.js
+++ src/Components/To-Do.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { Scrollbars } from 'react-custom-scrollbars';
-import TitleBar from './TitleBar';
-import FilterSwitches from './FilterSwitches';
 
-function TaskDetails(props) {
+export function TaskDetails(props) {
     const hour = Math.trunc(props.todoItem.workTime/60);
     const minute = props.todoItem.workTime%60;
     const workStat = hour == 0 ? "Worked "+minute+" minutes on this task."
@@ -27,26 +24,16 @@ function TaskDetails(props) {
     );
 }
 
-function InputForm(props) {
+export function InputForm(props) {
   return(
-      <Fade view={props.view}>
         <form onSubmit={props.handleSubmit}>
           <input type="text" onChange={props.handleChange} value={props.stateValue} />
           <button>Add To-Do</button>
         </form>
-      </Fade>
   )
 }
 
-function Fade(props) {
-  return(
-    <CSSTransition in={props.view} timeout={1000} classNames="fade">
-      {props.children}
-    </CSSTransition>
-  )
-}
-
-function TodoList(props) {
+export function TodoList(props) {
   let list = props.todoList;
 
   if (props.filter == "undone") {
@@ -78,139 +65,4 @@ function TodoList(props) {
   );
 }
 
-class TodoApp extends Component {
-  constructor(props) {
-    super(props);
-    if (localStorage.getItem('todos') == null) {
-      localStorage.setItem('todos', JSON.stringify([]));
-    }
-
-    this.state = {
-      value: '',
-      todoList: JSON.parse(localStorage['todos']),
-      filter: "undone",
-      toggleInput: false,
-      toggleFilter: false
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.removeTask = this.removeTask.bind(this);
-    this.markDone = this.markDone.bind(this);
-    this.expandTask = this.expandTask.bind(this);
-    this.filterList = this.filterList.bind(this);
-    this.toggleInput = this.toggleInput.bind(this);
-    this.toggleFilter = this.toggleFilter.bind(this);
-    
-  }
-
-  handleChange(event) {
-    this.setState({
-      value: event.target.value
-    });  
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const currentItem = {
-      id: this.state.todoList.length + 1,
-      title: this.state.value,
-      isDone: false,
-      description: '',
-      workTime: 0
-    };
-
-    if(this.state.value != '') {
-      this.setState({
-        todoList: this.state.todoList.concat(currentItem),
-        value: ''
-      });
-    }    
-  }
-
-  markDone = param => event => {
-    const newList = this.state.todoList.map( item => {
-      if (item.id == param) {
-        return Object.assign({}, item, {isDone: !item.isDone});
-      }
-      return item;
-    });
-
-    this.setState({
-      todoList: newList
-    });
-  }
-
-  removeTask = param => event => {
-    const { todoList } = this.state;
-    this.setState({
-      todoList: todoList.filter(
-        item => item.id != param
-      )
-    });
-  }
-
-  expandTask = param => event => {
-    const ele = "taskDetails" + param;
-    document.getElementById(ele).classList.toggle("expanded");
-    event.target.classList.toggle("selected");
-  }
-
-  handleDescription = param => event => {
-    const {todoList} = this.state;
-    this.setState({
-      todoList: todoList.map(
-        item => (
-          item.id == param ? Object.assign({}, item, {description: event.target.value})
-        : item ) 
-      )
-    });
-  }
-
-  filterList(event) {
-    event.preventDefault();
-
-    //Adding/removing class for highlighting
-    document.querySelector("a.active").classList.remove("active");
-    event.target.classList.add("active");
-
-    this.setState({
-        filter: event.target.name
-    })
-  }
-
-  toggleInput(event) {
-    event.target.classList.toggle("active");
-    this.setState({
-      toggleInput: !this.state.toggleInput
-    })
-  }
-
-  toggleFilter(event) {
-    event.target.classList.toggle("active");
-    this.setState({
-      toggleFilter: !this.state.toggleFilter
-    })
-  }
-
-  componentDidUpdate() {
-    localStorage.setItem('todos', JSON.stringify(this.state.todoList));
-  }
-
-  render() {
-    return(
-      <Scrollbars style={{width: 450, height: 400}}> 
-        <div className="todo-box">
-          <TitleBar title="ToDo List" toggleInput={this.toggleInput} toggleFilter={this.toggleFilter}/>
-          {this.state.toggleInput && <InputForm handleSubmit={this.handleSubmit} handleChange={this.handleChange} stateValue={this.state.value} view={this.state.toggleInput}/>}
-          {this.state.toggleFilter && <FilterSwitches filterList={this.filterList}/>}
-          <TodoList todoList={this.state.todoList} filter={this.state.filter} markDone={this.markDone}
-          removeTask={this.removeTask} expandTask={this.expandTask}
-          handleDescription={this.handleDescription}/>
-        </div>
-      </Scrollbars>
-    );
-  }
-}
-
-export default TodoApp;
+export default TodoList;
