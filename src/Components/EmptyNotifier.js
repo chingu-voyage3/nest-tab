8e4aebbb93244f2b3ed7680484bd7cc355ebc226
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-02-22 00:19:10 +0600

    added sad emoticons in heading

--- src/Components/EmptyNotifier.js
+++ src/Components/EmptyNotifier.js
@@ -4,7 +4,7 @@ import '../styles/EmptyNotifier.css';
 function EmptyNotifier(props) {
     return(
         <div className="empty">
-            <h3>Whhow! Such emptiness!</h3>
+            <h3>Whhow! Such emptiness! <i className="material-icons">sentiment_very_dissatisfied</i></h3>
             <p>Click the <i className="material-icons">add</i> button to add a new item.</p>
         </div>
     )
